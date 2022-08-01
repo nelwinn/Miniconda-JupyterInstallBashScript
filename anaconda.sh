77c86c23fdc63911ef1e0aeee3a32786f638f943
nelwinn <nelwinnn@gmail.com>
2022-08-01 10:42:38 +0530

    update

--- anaconda.sh
+++ anaconda.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sudo apt-get install ca-certificates
+sudo apt install curl
 export CURL_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt
 curl -o cond.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 
@@ -9,8 +10,8 @@ chmod +x cond.sh
 ./cond.sh
 
 export PATH="/home/username/miniconda/bin:$PATH"
-echo "Miniconda is now installed, reopen your terminal"
+echo "Miniconda is now installed, close and reopen your terminal"
 
-echo -ne '\n\nTo create a conda environment, type conda create -n "name for env" python="version"\n\nTo activate an environment, conda activate "name for env"\nYou can then proceed to install jupyterlab using\n\t conda install jupyterlab\n\n`N`'
+echo -ne '\n\nTo create a conda environment, type conda create -n "name for env" python="version"\n\nTo activate an environment, conda activate "name for env"\nYou can then proceed to install jupyterlab using\n\t conda install jupyterlab\n\n\t`N`\n'
 
 
